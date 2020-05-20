d0b65f55176c89fbfdeaeff1c2f1769a97fffb04
Alejandro Rojas <dorukotokyo@gmail.com>
2020-05-20 14:05:36 +0200

    Added notification system

--- app/src/main/java/me/alejandro/mtgspoileralert/data/worker/SyncDataWorker.kt
+++ app/src/main/java/me/alejandro/mtgspoileralert/data/worker/SyncDataWorker.kt
@@ -1,10 +1,17 @@
 package me.alejandro.mtgspoileralert.data.worker
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat.getSystemService
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import me.alejandro.mtgspoileralert.R
 import me.alejandro.mtgspoileralert.data.usecases.GetCardsUseCase
 import me.alejandro.mtgspoileralert.domain.base.Failure
 import me.alejandro.mtgspoileralert.domain.model.card.Card
@@ -12,11 +19,13 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class SyncDataWorker @Inject constructor(
-    context: Context,
+    val context: Context,
     params: WorkerParameters,
-    val getCardsUseCase: GetCardsUseCase
+    private val getCardsUseCase: GetCardsUseCase
 ) :
     CoroutineWorker(context, params) {
+
+    val CHANNEL_ID = "cardsId"
 
     override suspend fun doWork(): Result = withContext(Dispatchers.IO) {
         try {
@@ -31,11 +40,32 @@ class SyncDataWorker @Inject constructor(
     }
 
     private fun handleFailure(failure: Failure) {
-
+        //Ignored
     }
 
     private fun handleSuccess(list: List<Card>) {
+        createNotificationChannel()
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.mipmap.ic_launcher_round)
+            .setContentTitle(context.resources.getString(R.string.new_cards_notification_title))
+            .setContentText(context.resources.getString(R.string.new_cards_notification_content))
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
+        with(NotificationManagerCompat.from(context)) {
+            notify(1, builder.build())
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "New cards notifications"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+
+            val notificationManager: NotificationManager =
+                getSystemService(context, NotificationManager::class.java) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
     }
 
     class Factory @Inject constructor(
